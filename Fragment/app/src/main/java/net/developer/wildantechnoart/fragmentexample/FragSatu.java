91dad7e7d14ae18553d0082d19724026c321c95c
anggunvrilia <65345432+anggunvrilia@users.noreply.github.com>
2020-05-15 23:15:46 +0800

    Add files via upload

---
+++ Fragment/app/src/main/java/net/developer/wildantechnoart/fragmentexample/FragSatu.java
@@ -0,0 +1,19 @@
+package net.developer.wildantechnoart.fragmentexample;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragSatu extends Fragment {
+
+    public FragSatu () {
+
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View viewFrag1 = inflater.inflate(R.layout.activity_frag_satu, container, false);
+        return viewFrag1;
+    }
+}
